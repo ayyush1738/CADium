31e07317fdf477f48ab19fd8679d7956b81955b2
unknown <singhrathoreayush824@gmail.com>
2025-03-06 22:20:52 +0530

    latest

---
+++ backend/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, send_from_directory, jsonify
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Upload file route
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(filepath)
+    return jsonify({"message": "File uploaded", "filename": file.filename}), 200
+
+# Retrieve 3D model route
+@app.route('/models/<filename>', methods=['GET'])
+def get_model(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ cad-frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>3D CAD Viewer</h1>
+    <FileUpload @fileUploaded="setFilename" />
+    <ModelViewer v-if="filename" :filename="filename" />
+  </div>
+</template>
+
+<script>
+import FileUpload from "./components/FileUpload.vue";
+import ModelViewer from "./components/ModelViewer.vue";
+
+export default {
+  components: { FileUpload, ModelViewer },
+  data() {
+    return {
+      filename: "",
+    };
+  },
+  methods: {
+    setFilename(name) {
+      this.filename = name;
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
